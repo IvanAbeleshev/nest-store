224e6bd8647895d7eb789ad0c79c06bd3373320f
Abeleshev Ivan <abeleshev1993@gmail.com>
2024-01-21 23:40:00 +0200

    added API for product categories

--- src/product/category/category.controller.ts
+++ src/product/category/category.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, Res, UseGuards } from '@nestjs/common';
+import { AdminRoleGuard } from 'src/auth/guards/admin-role/admin-role.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { CreateCategoryDTO } from './dto/create-category.dto';
+import { CategoryService } from './category.service';
+import { SetCategoryInternalizationDTO } from './dto/set-category-internalization.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+  constructor(private categoryService: CategoryService){}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(JwtAuthGuard, AdminRoleGuard)
+  async createProduct(@Body() body:CreateCategoryDTO, @Res() res: Response){
+    const category = await this.categoryService.create(body)
+    return res.json()
+  }
+
+  @Post('translation/:id')
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(JwtAuthGuard, AdminRoleGuard)
+  async setCategoryInternalization(
+    @Param('id') id: string, 
+    @Body() body: SetCategoryInternalizationDTO, 
+    @Res() res: Response)
+  {
+    const categoryId = await this.categoryService.find(id)
+    const categoryTranslation = await this.categoryService.createCategoryDescription(body, categoryId)
+
+    return res.json()
+  }
+}
--- src/product/category/category.service.ts
+++ src/product/category/category.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDTO } from './dto/create-category.dto';
+import { SetCategoryInternalizationDTO } from './dto/set-category-internalization.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+
+  constructor(private prisma: PrismaService){}
+
+  private async createCategoryDescriptions(descriptionsDTO: SetCategoryInternalizationDTO[], categoryID:number){
+    const description = await this.prisma.productCategoryInternalization.createMany({
+      data:descriptionsDTO.map(el=>(
+        {
+          ...el, 
+          productCategoryId: categoryID
+        }
+      ))
+    })
+  }
+
+  async createCategoryDescription(descriptionDTO: SetCategoryInternalizationDTO, categoryID:number){
+    const description = await this.prisma.productCategoryInternalization.createMany({
+      data:{
+        ...descriptionDTO,
+        productCategoryId: categoryID
+      }
+    })
+  }
+
+  async create(categoryDTO: CreateCategoryDTO){
+    const category = await this.prisma.productCategory.create({
+      data:{
+        UID: categoryDTO.UID
+      }
+    })
+
+    if(categoryDTO.descriptions){
+      this.createCategoryDescriptions(categoryDTO.descriptions, category.id)
+    }
+
+    return category
+  }
+
+  async find(idOrUID:string):Promise<number>{
+    let id = null
+    try{
+      id = Number(idOrUID)
+    }catch(error){
+
+    }
+    const product = await this.prisma.productCategory.findFirst({
+      where:{
+        OR:[
+          {id},
+          {UID: idOrUID}
+        ]
+      }
+    })
+
+    if(!product){
+      throw new BadRequestException('product category does not exist')
+    }
+    
+    return product.id
+  }
+}
